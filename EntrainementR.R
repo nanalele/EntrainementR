64778903b0f61053b77862408c590579f655bac4
Léa J <jiletlea@gmail.com>
2018-11-05 17:50:16 +0100

    ENtrainement R - Version 1.2 (05/11/2018)

--- EntrainementR.R
+++ EntrainementR.R
@@ -1,3 +1,8 @@
 # Importation des données
     
     calcium<-read.csv2("dataTPcalcium.csv")
+    QI<-read.csv2("dataTPQI.csv")
+    retinol<-read.csv2("dataTPretinol.csv")
+    USI<-read.csv2("dataTPUSI.csv")
+    valves<-read.csv2("dataTPvalves.csv")
+    
